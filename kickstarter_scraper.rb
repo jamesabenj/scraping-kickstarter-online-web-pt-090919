002c7332d7fd69a8da3e073e7296bba132d6fe5e
James Benjamin <james.abenj@gmail.com>
2019-10-22 22:23:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,4 +2,18 @@
 
 def create_project_hash
   # write your code here
+  :projects => {
+  "My Great Project"  => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  },
+  "Another Great Project" => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  }
+}
 end
